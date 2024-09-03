87d5a9b10e141f0066ab0be0cce1f6e76f413158
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2024-09-03 10:46:57 +0900

    Update Floor.java

--- src/Floor.java
+++ src/Floor.java
@@ -1,4 +1,18 @@
 package PACKAGE_NAME;
 
 public class Floor {
+    private double width;
+    private double length;
+
+    public Floor(double width, double height) {
+        if (width < 0 ){width = 0;}
+        if (height < 0 ){height = 0;}
+
+        this.width = width;
+        this.length = height;
+    }
+
+    public double getArea(){
+        return this.width * this.length;
+    }
 }
