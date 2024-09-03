ef8264712db3cecb78ea71eb6d40de9fe240c955
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2024-09-03 10:46:29 +0900

    Update Carpet.java

--- src/Carpet.java
+++ src/Carpet.java
@@ -1,4 +1,14 @@
 package PACKAGE_NAME;
 
 public class Carpet {
+    private double cost;
+
+    public Carpet(double cost) {
+        if(cost < 0){cost = 0;}
+        this.cost = cost;
+    }
+
+    public double getCost(){
+        return this.cost;
+    }
 }
